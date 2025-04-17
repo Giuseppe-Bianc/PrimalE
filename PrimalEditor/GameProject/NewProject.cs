3e8c23c524e6198653cc216e36c6b900f1b241fe
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2025-04-17 18:15:53 +0200

    feat: Implement MVVM pattern with ViewModelBase and NewProject
    Added the `ViewModelBase` class to implement the `INotifyPropertyChanged` interface, enabling property change notifications. This serves as a base class for view models in the application.
    
    Created the `NewProject` class that inherits from `ViewModelBase`, managing two properties: `Name` and `Path`. Each property includes logic to notify changes, ensuring the UI stays in sync with the underlying data.
    
    Updated `NewProjectView.xaml` to bind the `TextBox` controls for `Name` and `Path` properties, allowing for direct interaction with the view model. This enhances the user experience by providing real-time updates in the UI based on user input.

---
+++ PrimalEditor/GameProject/NewProject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimalEditor.GameProject {
+	public class NewProject : ViewModelBase {
+		private string _name = "NewProject";
+
+		public string Name {
+			get => _name;
+			set {
+				if (_name != value) {
+					_name = value;
+					OnPropertyChanged(nameof(Name));
+				}
+			}
+		}
+
+		private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProject\";
+
+		public string Path {
+			get => _path;
+			set {
+				if (_path != value) {
+					_path = value;
+					OnPropertyChanged(nameof(Path));
+				}
+			}
+		}
+
+	}
+}
+
