bce338afc3d79a391ffb225ba8029fafbdd3695d
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2025-04-18 11:53:08 +0200

    feat: Introduce ProjectTemplate management and UI updates
    Added a new class `ProjectTemplate` in `NewProject.cs` to manage project details such as project type, project file, folders, icon, and screenshot. Updated `NewProject` properties to use `ProjectName` and `ProjectPath` instead of `Name` and `Path` for better clarity. An observable collection of `ProjectTemplate` was added to handle project templates, and the constructor now loads a list of XML template files along with their icons and screenshots.
    
    Updated the UI in `NewProjectView.xaml` to bind a `ListBox` to `ProjectTemplates`, displaying the project icon and type. The binding properties for project name and path were also updated to reflect the new property names, enhancing user experience.
    
    Additionally, a new file `template.xml` was created to define a project template with necessary folders, project file, and project type, facilitating better project management within the application.

--- PrimalEditor/GameProject/NewProject.cs
+++ PrimalEditor/GameProject/NewProject.cs
@@ -1,35 +1,79 @@
-﻿using System;
+﻿using PrimalEditor.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PrimalEditor.GameProject {
+	[DataContract]
+	public class ProjectTemplate {
+		[DataMember]
+		public string ProjectType { get; set; }
+		[DataMember]
+		public string ProjectFile { get; set; }
+		[DataMember]
+		public List<string> Folders { get; set; }
+
+		public byte[] Icon { get; set; }
+		public byte[] Screenshot { get; set; }
+		public string IconFilePath { get; set; }
+		public string ScreenshotFilePath { get; set; }
+		public string ProjectFilePath { get; set; }
+	}
 	public class NewProject : ViewModelBase {
-		private string _name = "NewProject";
+		private readonly string _templatePath = $@"..\..\PrimalEditor\ProjectTemplates";
+		private string _projectName = "NewProject";
 
-		public string Name {
-			get => _name;
+		public string ProjectName {
+			get => _projectName;
 			set {
-				if (_name != value) {
-					_name = value;
-					OnPropertyChanged(nameof(Name));
+				if (_projectName != value) {
+					_projectName = value;
+					OnPropertyChanged(nameof(ProjectName));
 				}
 			}
 		}
 
-		private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProject\";
+		private string _projectpath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProject\";
 
-		public string Path {
-			get => _path;
+		public string ProjectPath {
+			get => _projectpath;
 			set {
-				if (_path != value) {
-					_path = value;
-					OnPropertyChanged(nameof(Path));
+				if (_projectpath != value) {
+					_projectpath = value;
+					OnPropertyChanged(nameof(ProjectPath));
 				}
 			}
 		}
 
+		private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+		public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
+
+		public NewProject() {
+			ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+			try {
+				var templatesFiles = System.IO.Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+				Debug.Assert(templatesFiles.Any());
+				foreach (var file in templatesFiles) {
+					var temaplate = Serializer.FromFile<ProjectTemplate>(file);
+					temaplate.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "icon.png"));
+					temaplate.Icon = File.ReadAllBytes(temaplate.IconFilePath);
+					temaplate.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Screenshot.png"));
+					temaplate.Screenshot = File.ReadAllBytes(temaplate.ScreenshotFilePath);
+					temaplate.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), temaplate.ProjectFile));
+					_projectTemplates.Add(temaplate);
+				}
+			} catch (Exception ex) {
+				// Handle exceptions here
+				Console.WriteLine($"Error: {ex.Message}");
+			}
+
+		}
 	}
 }
 
